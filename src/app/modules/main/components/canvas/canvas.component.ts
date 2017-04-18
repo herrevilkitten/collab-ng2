d1565310d0c43ba8dfb71433aa802b601515f226
Eric Kidder <eric.kidder@pgi.com>
2017-04-18 12:42:45 -0400

    Basic support for additional drawing shapes.

--- src/app/modules/main/components/canvas/canvas.component.ts
+++ src/app/modules/main/components/canvas/canvas.component.ts
@@ -16,6 +16,7 @@ export class CanvasComponent implements OnInit {
 
   mouseDown = false;
   path: svgjs.Path;
+  shape: svgjs.Shape;
   lastNode = '';
 
   constructor() { }
@@ -33,37 +34,124 @@ export class CanvasComponent implements OnInit {
     const x = $event.offsetX;
     const y = $event.offsetY;
 
-    if (this.canvasTools.value === 'pencil') {
-      this.lastNode = 'M' + x + ' ' + y;
-      this.path = this.svg.svg
-        .path(this.lastNode)
-        .attr({
-          fill: 'none',
-          stroke: '#000',
-          'stroke-width': 1
-        });
+    switch (this.canvasTools.value) {
+      case 'pencil':
+        this.lastNode = 'M' + x + ' ' + y;
+        this.path = this.svg.svg
+          .path(this.lastNode)
+          .attr({
+            fill: 'none',
+            stroke: '#000',
+            'stroke-width': 1
+          });
+        break;
+      case 'line':
+        this.shape = this.svg.svg
+          .line(x, y, x, y)
+          .attr({
+            fill: '#000',
+            stroke: '#000',
+            x2: x,
+            y2: y,
+            originalX: x,
+            originalY: y,
+          });
+        break;
+      case 'rectangle':
+        this.shape = this.svg.svg
+          .rect(1, 1)
+          .attr({
+            fill: '#000',
+            stroke: '#000',
+            'stroke-width': 1,
+            originalX: x,
+            originalY: y,
+          })
+          .move(x, y);
+        break;
+      case 'ellipse':
+        this.shape = this.svg.svg
+          .ellipse(1, 1)
+          .attr({
+            fill: '#000',
+            stroke: '#000',
+            'stroke-width': 1,
+            originalX: x,
+            originalY: y,
+          })
+          .move(x, y);
+        break;
     }
   }
 
   onMouseUp($event: MouseEvent) {
     this.mouseDown = false;
+    this.path = null;
+    this.shape = null;
   }
 
   onMove($event: MouseEvent) {
     console.log($event);
     const x = $event.offsetX;
     const y = $event.offsetY;
+    let originalX;
+    let originalY;
+    let height;
+    let width;
 
-    if (this.mouseDown) {
-      if (this.canvasTools.value === 'pencil') {
-        this.lastNode = 'L' + x + ' ' + y;
-        this.path
-          .attr({
-            d: (this.path.attr('d') || '') + ' ' + this.lastNode,
-            fill: 'none',
+    if (this.mouseDown && (this.path || this.shape)) {
+      switch (this.canvasTools.value) {
+        case 'pencil':
+          this.lastNode = 'L' + x + ' ' + y;
+          this.path
+            .attr({
+              d: (this.path.attr('d') || '') + ' ' + this.lastNode,
+              fill: 'none',
+              stroke: '#000',
+              'stroke-width': 1
+            });
+          break;
+        case 'line':
+          this.shape.attr({
+            fill: '#000',
             stroke: '#000',
-            'stroke-width': 1
+            x2: x,
+            y2: y,
           });
+          break;
+        case 'rectangle':
+          originalX = this.shape.attr('originalX');
+          originalY = this.shape.attr('originalY');
+          width = Math.abs(originalX - x);
+          height = Math.abs(originalY - y);
+          let newX = originalX;
+          let newY = originalY;
+          if (originalX > x) {
+            newX = x;
+          }
+          if (originalY > y) {
+            newY = y;
+          }
+          if (newX !== originalX || newY !== originalY) {
+            this.shape.move(newX, newY);
+          }
+          this.shape.size(width, height);
+          this.shape.attr({
+            fill: '#000',
+            stroke: '#000',
+          });
+          break;
+        case 'ellipse':
+          originalX = this.shape.attr('originalX');
+          originalY = this.shape.attr('originalY');
+          width = Math.abs(originalX - x) * 2;
+          height = Math.abs(originalY - y) * 2;
+          this.shape.size(width, height);
+          this.shape.attr({
+            fill: '#000',
+            stroke: '#000',
+          });
+          break;
       }
     }
   }
