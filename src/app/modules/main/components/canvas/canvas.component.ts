946789a0b1e8652c01167ee8faa8e22ca7ba3643
Eric Kidder <eric.kidder@pgi.com>
2017-04-19 06:52:57 -0400

    * Added ability to create a square by holding down shift when drawing a rectangle

--- src/app/modules/main/components/canvas/canvas.component.ts
+++ src/app/modules/main/components/canvas/canvas.component.ts
@@ -3,6 +3,175 @@ import { MdSidenav, MdButtonToggleGroup } from '@angular/material';
 
 import { SvgJsDirective } from '../../../../directives/svg-js.directive';
 
+import * as SVG from 'svg.js';
+
+export abstract class CanvasShape {
+  original: any;
+
+  constructor(protected svg: svgjs.Doc) {
+
+  }
+
+  startDrawing(event: MouseEvent): this { return this; }
+
+  continueDrawing(event: MouseEvent): this { return this; }
+
+  endDrawing(event: MouseEvent): this { return this; }
+
+  protected parseMouseEvent(event: MouseEvent) {
+    return {
+      x: event.offsetX,
+      y: event.offsetY,
+    };
+  }
+
+  toJSON() {
+    return JSON.stringify(this);
+  }
+}
+
+export class PencilShape extends CanvasShape {
+  lastNode = '';
+
+  startDrawing(event: MouseEvent) {
+    const { x, y } = this.parseMouseEvent(event);
+    this.lastNode = 'M' + x + ' ' + y;
+    this.original = this.svg
+      .path(this.lastNode)
+      .attr({
+        fill: 'none',
+        stroke: '#000',
+        'stroke-width': 1
+      });
+    return this;
+  }
+
+  continueDrawing(event: MouseEvent) {
+    const { x, y } = this.parseMouseEvent(event);
+    this.lastNode = 'L' + x + ' ' + y;
+    this.original
+      .attr({
+        d: (this.original.attr('d') || '') + ' ' + this.lastNode,
+        fill: 'none',
+        stroke: '#000',
+        'stroke-width': 1
+      });
+    return this;
+  }
+}
+
+export class LineShape extends CanvasShape {
+  startDrawing(event: MouseEvent) {
+    const { x, y } = this.parseMouseEvent(event);
+    this.original = this.svg
+      .line(x, y, x, y)
+      .attr({
+        fill: '#000',
+        stroke: '#000',
+        x2: x,
+        y2: y,
+        originalX: x,
+        originalY: y,
+      });
+    return this;
+  }
+
+  continueDrawing(event: MouseEvent) {
+    const { x, y } = this.parseMouseEvent(event);
+    this.original.attr({
+      fill: '#000',
+      stroke: '#000',
+      x2: x,
+      y2: y,
+    });
+    return this;
+  }
+}
+
+export class RectangleShape extends CanvasShape {
+  startDrawing(event: MouseEvent) {
+    const { x, y } = this.parseMouseEvent(event);
+    this.original = this.svg
+      .rect(1, 1)
+      .attr({
+        fill: '#000',
+        stroke: '#000',
+        'stroke-width': 1,
+        originalX: x,
+        originalY: y,
+      })
+      .move(x, y);
+    return this;
+  }
+
+  continueDrawing(event: MouseEvent) {
+    const { x, y } = this.parseMouseEvent(event);
+    const originalX = this.original.attr('originalX');
+    const originalY = this.original.attr('originalY');
+    let width = Math.abs(originalX - x);
+    let height = Math.abs(originalY - y);
+    let newX = originalX;
+    let newY = originalY;
+    if (event.shiftKey) {
+      height = width = Math.min(height, width);
+      if (originalX > x) {
+        newX = originalX - width;
+      }
+      if (originalY > y) {
+        newY = originalY - height;
+      }
+    } else {
+      if (originalX > x) {
+        newX = x;
+      }
+      if (originalY > y) {
+        newY = y;
+      }
+    }
+
+    if (newX !== originalX || newY !== originalY) {
+      this.original.move(newX, newY);
+    }
+    this.original.size(width, height);
+    this.original.attr({
+      fill: '#000',
+      stroke: '#000',
+    });
+    return this;
+  }
+}
+
+export class EllipseShape extends CanvasShape {
+  startDrawing(event: MouseEvent) {
+    const { x, y } = this.parseMouseEvent(event);
+    this.original = this.svg
+      .ellipse(1, 1)
+      .attr({
+        fill: '#000',
+        stroke: '#000',
+        'stroke-width': 1,
+        originalX: x,
+        originalY: y,
+      })
+      .move(x, y);
+    return this;
+  }
+
+  continueDrawing(event: MouseEvent) {
+    const { x, y } = this.parseMouseEvent(event);
+    const originalX = this.original.attr('originalX');
+    const originalY = this.original.attr('originalY');
+    const width = Math.abs(originalX - x) * 2;
+    const height = Math.abs(originalY - y) * 2;
+    this.original.size(width, height);
+    this.original.attr({
+      fill: '#000',
+      stroke: '#000',
+    });
+    return this;
+  }
+}
+
 @Component({
   selector: 'app-canvas',
   templateUrl: './canvas.component.html',
@@ -10,14 +179,12 @@ import { SvgJsDirective } from '../../../../directives/svg-js.directive';
   encapsulation: ViewEncapsulation.None,
 })
 export class CanvasComponent implements OnInit {
-  @ViewChild(SvgJsDirective) svg: SvgJsDirective;
+  @ViewChild(SvgJsDirective) svgJs: SvgJsDirective;
   @ViewChild('canvasSidebar') sidebar: MdSidenav;
   @ViewChild('canvasToolGroup') canvasTools: MdButtonToggleGroup;
 
   mouseDown = false;
-  path: svgjs.Path;
-  shape: svgjs.Shape;
-  lastNode = '';
+  shape: CanvasShape = null;
 
   constructor() { }
 
@@ -34,125 +201,37 @@ export class CanvasComponent implements OnInit {
     const x = $event.offsetX;
     const y = $event.offsetY;
 
+    if (this.shape) {
+      this.shape.endDrawing($event);
+    }
+
     switch (this.canvasTools.value) {
       case 'pencil':
-        this.lastNode = 'M' + x + ' ' + y;
-        this.path = this.svg.svg
-          .path(this.lastNode)
-          .attr({
-            fill: 'none',
-            stroke: '#000',
-            'stroke-width': 1
-          });
+        this.shape = new PencilShape(this.svgJs.svg).startDrawing($event);
         break;
       case 'line':
-        this.shape = this.svg.svg
-          .line(x, y, x, y)
-          .attr({
-            fill: '#000',
-            stroke: '#000',
-            x2: x,
-            y2: y,
-            originalX: x,
-            originalY: y,
-          });
+        this.shape = new LineShape(this.svgJs.svg).startDrawing($event);
         break;
       case 'rectangle':
-        this.shape = this.svg.svg
-          .rect(1, 1)
-          .attr({
-            fill: '#000',
-            stroke: '#000',
-            'stroke-width': 1,
-            originalX: x,
-            originalY: y,
-          })
-          .move(x, y);
+        this.shape = new RectangleShape(this.svgJs.svg).startDrawing($event);
         break;
       case 'ellipse':
-        this.shape = this.svg.svg
-          .ellipse(1, 1)
-          .attr({
-            fill: '#000',
-            stroke: '#000',
-            'stroke-width': 1,
-            originalX: x,
-            originalY: y,
-          })
-          .move(x, y);
+        this.shape = new EllipseShape(this.svgJs.svg).startDrawing($event);
         break;
     }
   }
 
   onMouseUp($event: MouseEvent) {
     this.mouseDown = false;
-    this.path = null;
+    if (this.shape) {
+      this.shape.endDrawing($event);
+    }
     this.shape = null;
   }
 
   onMove($event: MouseEvent) {
-    console.log($event);
-    const x = $event.offsetX;
-    const y = $event.offsetY;
-    let originalX;
-    let originalY;
-    let height;
-    let width;
-
-    if (this.mouseDown && (this.path || this.shape)) {
-      switch (this.canvasTools.value) {
-        case 'pencil':
-          this.lastNode = 'L' + x + ' ' + y;
-          this.path
-            .attr({
-              d: (this.path.attr('d') || '') + ' ' + this.lastNode,
-              fill: 'none',
-              stroke: '#000',
-              'stroke-width': 1
-            });
-          break;
-        case 'line':
-          this.shape.attr({
-            fill: '#000',
-            stroke: '#000',
-            x2: x,
-            y2: y,
-          });
-          break;
-        case 'rectangle':
-          originalX = this.shape.attr('originalX');
-          originalY = this.shape.attr('originalY');
-          width = Math.abs(originalX - x);
-          height = Math.abs(originalY - y);
-          let newX = originalX;
-          let newY = originalY;
-          if (originalX > x) {
-            newX = x;
-          }
-          if (originalY > y) {
-            newY = y;
-          }
-          if (newX !== originalX || newY !== originalY) {
-            this.shape.move(newX, newY);
-          }
-          this.shape.size(width, height);
-          this.shape.attr({
-            fill: '#000',
-            stroke: '#000',
-          });
-          break;
-        case 'ellipse':
-          originalX = this.shape.attr('originalX');
-          originalY = this.shape.attr('originalY');
-          width = Math.abs(originalX - x) * 2;
-          height = Math.abs(originalY - y) * 2;
-          this.shape.size(width, height);
-          this.shape.attr({
-            fill: '#000',
-            stroke: '#000',
-          });
-          break;
-      }
+    if (this.mouseDown && this.shape) {
+      this.shape.continueDrawing($event);
     }
   }
 }
